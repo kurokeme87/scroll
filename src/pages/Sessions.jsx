855e49b052486bbe0e50c8ad39282db443b3c9fd
d3uceY <onyekwelujesse1234@gmail.com>
2024-10-08 16:56:38 +0100

    i am done, lol

--- src/pages/Sessions.jsx
+++ src/pages/Sessions.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './css/sessions.css'
+import ConnectWalletBtn from '../components/button/ConnectWalletBtn'
 
 const Sessions = () => {
 
@@ -162,36 +163,11 @@ const Sessions = () => {
                         >
                             --
                         </p>
-                        <div className="MuiBox-root css-4fl0bd-wrapper e1de0imv0">
-                            <span
-                                className="MuiButtonBase-root Mui-disabled MuiIconButton-root Mui-disabled MuiIconButton-sizeMedium e1de0imv0 css-wgwcyq-icon e1de0imv0"
-                                tabindex="-1"
-                                role="button"
-                                aria-disabled="true"
-                            ><svg
-                                fill="none"
-                                height="16"
-                                viewBox="0 0 21 16"
-                                width="21"
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium css-1jbxkcd e1de0imv0"
-                                focusable="false"
-                                aria-hidden="true"
-                            >
-                                    <path
-                                        d="m21 8.00267-8.3996 7.99733v-2.7067l4.5767-4.35752c-5.7251-.08013-11.45204.31876-17.1565267.05877l-.02057369-1.96951c5.68391039.23862 11.34730039-.1389 17.03120039-.06055l-4.4308-4.21858v-2.74591055l8.3996 7.99733055z"
-                                        fill="currentColor"
-                                    ></path></svg
-                                ></span>
-                            <div className="css-fs25ky-mask" style={{ width: '4.8rem' }}></div>
-                            <button
-                                className="MuiButtonBase-root css-6k4tt-button e1de0imv0"
-                                tabindex="0"
-                                type="button"
-                            >
-                                Connect Wallet
-                            </button>
-                        </div>
+
+                        <ConnectWalletBtn>
+                            Connect Wallet
+                        </ConnectWalletBtn>
+
                     </div>
                     <div className="MuiStack-root css-1rwpk26 e1de0imv0">
                         <p
@@ -1300,12 +1276,12 @@ const Sessions = () => {
                                             <div
                                                 className="MuiAvatar-root MuiAvatar-circular css-1mm627t e1de0imv0"
                                             >
-                                                
+
                                             </div>
                                             <div
                                                 className="MuiAvatar-root MuiAvatar-circular css-1mm627t e1de0imv0"
                                             >
-                                                
+
                                             </div></div></span>
                                 </div>
                                 <div
